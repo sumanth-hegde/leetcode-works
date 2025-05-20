e4c7a0b1515c366d5767cbd13a59b5743cd625b7
sumanth-hegde <sumanthsh1998@gmail.com>
2025-05-20 15:49:48 +0530

    largest in an array optimised

--- largest_in_array.py
+++ largest_in_array.py
@@ -1,8 +1,6 @@
 def largest_element(arr):
     largest = arr[0]
-    for i in range(len(arr)):
-        if arr[i] > largest:
-            largest = arr[i]
+    [largest := arr[i] for i in range(len(arr)) if arr[i] > largest]
     return largest
 
 
