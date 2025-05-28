91219f0ef49881000d5e01f0a071c744e98c399b
sumanth-hegde <sumanthsh1998@gmail.com>
2025-05-28 22:47:15 +0530

    maximum sub array

---
+++ medium_questions/maximum_subarray.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr_sum = max_sum = nums[0]
+        for num in nums[1:]:
+            curr_sum = max(num, curr_sum + num)
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
+
+
+sol = Solution()
+print(sol.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
